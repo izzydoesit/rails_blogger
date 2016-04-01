c86ccdbcb5d83e9cc679fa2e29adff594615bfba
Israel Matos <israeldmatos@gmail.com>
2016-03-31 22:36:43 -0700

    first blogger commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '694fe058c7ea1ee4da2857abb128c45943cccbd95442214387086d3ed7441916cb0ed694b380418f0e8cecde9919cf393ca50ef35900ea0ae18b8fe677b5b8b9'
